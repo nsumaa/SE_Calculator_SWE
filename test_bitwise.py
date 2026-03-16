39dc2a8aefa91db2b20e60253ae388a2e037e592
akshay060805 <vanama.akshay06@gmail.com>
2026-03-16 14:06:41 +0530

    FeatureA_5 added test cases

--- test_bitwise.py
+++ test_bitwise.py
@@ -12,14 +12,23 @@ class TestBitwise(unittest.TestCase):
     def test_bitwise_or(self):
         self.assertEqual(self.calc.calculate("12 | 10"), 14)
 
+    def test_bitwise_xor(self):
+        self.assertEqual(self.calc.calculate("12 ^ 10"), 6)
+
     def test_bitwise_lshift(self):
         self.assertEqual(self.calc.calculate("3 << 2"), 12)
+
+    def test_bitwise_lshift(self):
+        self.assertEqual(self.calc.calculate("3 >> 2"), 0)
 
     def test_bitwise_not(self):
         self.assertEqual(self.calc.calculate("~0"), -1)
 
     def test_mixed_expression(self):
         self.assertEqual(self.calc.calculate("2 + (1 << 3)"), 10)
+
+    def test_bitwise_not_2(self):
+        self.assertNotEqual(self.calc.calculate("~0"), 0)
 
 if __name__ == "__main__":
     unittest.main()
